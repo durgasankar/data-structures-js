7db4ccb8dd84e556d4598b12677518fc8d38e56a
durgasankar <durgasankar.raja500@gmail.com>
2022-06-11 18:48:44 +0530

    added pop method with all possible validations for 0,1,more than 1 elements

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -25,11 +25,39 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        // having no data in the list
+        if (!this.head) return undefined;
+        // more than one data in the list
+        let prevNode = this.head;
+        let tempNode = this.head;
+        while (tempNode.next) {
+            prevNode = tempNode;
+            tempNode = tempNode.next;
+        }
+        this.tail = prevNode;
+        this.tail.next = null;
+        this.length--;
+        // if length is 0 it will points to default.
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+    }
 }
+
 
 const myList = new LinkedList(2);
 myList.push(4);
 myList.push(6);
 myList.push(8);
 myList.push(10);
+console.log(myList);
+myList.pop();
+myList.pop();
+myList.pop();
+// myList.pop();
+// myList.pop();
+// myList.pop();
 console.log(myList);
