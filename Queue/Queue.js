f31d027d2ed8badd3e2985f4af86a09056d62256
durgasankar <durgasankar.raja500@gmail.com>
2022-07-10 22:45:06 +0530

    added print method of queue

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -15,7 +15,7 @@ class Queue {
 
     enQueue(value) {
         const newNode = new Node(value);
-        if (!this.first) {
+        if (this.length === 0) {
             this.last = newNode;
         } else {
             newNode.next = this.first;
@@ -24,4 +24,27 @@ class Queue {
         this.length++;
         return this;
     }
+
+    print() {
+        let values = {};
+        let tempFirst = this.first;
+        let index = 0;
+        while (tempFirst) {
+            values[index] = tempFirst.value;
+            index++;
+            tempFirst = tempFirst.next;
+        }
+        return values;
+    }
 }
+
+const queue = new Queue(10);
+queue.enQueue(20);
+queue.enQueue(30);
+queue.enQueue(40);
+queue.enQueue(50);
+queue.enQueue(60);
+queue.enQueue(70);
+queue.enQueue(80);
+
+console.log(queue.print(), this.length);
